5b5b9ab0cf63fc394e2e2d5b334a66e3e69468a2
Aleksei Allen <aqml@mail.ru>
2021-11-02 14:37:31 +0300

    Add project files.

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork23
+{
+    // Разработать асинхронный метод для вычисления факториала числа.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение натурального числа для вычисления факториала числа");
+            int factInput = Convert.ToInt32(Console.ReadLine());
+
+            if (factInput == 0 || factInput == 1)
+            {
+                Console.WriteLine("Факториал {0} = 1", factInput);
+            }
+            else
+            {
+                Console.WriteLine("Факториал {0} = {1}", factInput, FactorialAsync(factInput).Result);
+            }
+            Console.ReadKey();
+        }
+        static BigInteger Factorial(int n)
+        {
+            BigInteger resultFactorial = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                resultFactorial *= i;
+            }
+            return resultFactorial;
+        }
+        static async Task<BigInteger> FactorialAsync(int n)
+        {
+            BigInteger result = await Task.Run(() => Factorial(n));
+            return result;
+        }
+    }
+}
